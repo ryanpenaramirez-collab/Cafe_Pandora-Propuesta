5a5fd28ddd2d9b9172f92fcce925e927caf53042
ignacio <progremb@gmail.com>
2026-06-10 21:30:31 -0500

    cambios en el index.ts

--- src/apartado-finanza/index.ts
+++ src/apartado-finanza/index.ts
@@ -1,8 +1,8 @@
 export { default as Facturacion } from './Facturacion';
 export { default as VentasDia } from './VentasDia';
-export { default as Informes } from './Informes';
-export { default as Cajero } from './Cajero';
-export { default as Egresos } from './Egresos';
+//export { default as Informes } from './Informes';
+//export { default as Cajero } from './Cajero';
+//export { default as Egresos } from './Egresos';
 export { default as AperturaCaja } from './AperturaCaja';
 export { default as FinanzasDashboard } from './FinanzasDashboard';
 export type { FinanzasTab } from './FinanzasDashboard';
